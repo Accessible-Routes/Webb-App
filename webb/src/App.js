d4253c9f923729dc13ab6c71b34d34e93bf394bd
json-mp3 <114618905+json-mp3@users.noreply.github.com>
2023-06-06 18:57:16 +0000

    hello

---
+++ webb/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          <MapComponent/> 
+        </a>
+      </header>
+    </div>
+  );
+
+
+}
+
+const MapComponent = () =>{
+  return(<div>
+    I am the map
+    </div>)
+
+}
+
+
+export default App;
