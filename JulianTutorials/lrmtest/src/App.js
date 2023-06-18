25190c10f71c3ddc125deb4110471fa52682d81e
Julian Parker-Sartori <132828842+becausej@users.noreply.github.com>
2023-06-18 18:31:06 +0000

    .

---
+++ JulianTutorials/lrmtest/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map2 from './Map';
+
+export default function App() {
+  return (
+    <div>
+      <Map2 />
+    </div>
+  );
+}
---
+++ JulianTutorials/lrmtest/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Map2 from "./Map";
+
+ReactDOM.render(<Map2 />, document.getElementById("root"));
