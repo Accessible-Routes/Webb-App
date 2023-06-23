08381e8678a7aed602331dbe9e08ffe4d9127e68
json-mp3 <114618905+json-mp3@users.noreply.github.com>
2023-06-23 17:52:54 +0000

    Fix merge conflict

--- webb/src/App.js
+++ webb/src/App.js
@@ -3,34 +3,15 @@ import MyMap from './common/components/mapSample.component';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <MyMap />
-      </header>
-=======
     <div className="Map-container">
       <div className="Map">
         <MyMap/>
       </div>
->>>>>>> 707a7095b8c95247fd226ef13ac3b03ab09897ad
     </div>
   );
 
 
 }
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 707a7095b8c95247fd226ef13ac3b03ab09897ad
 
 export default App;
