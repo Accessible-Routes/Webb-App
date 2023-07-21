88236234be455a7728af592a3982ace53d507295
Zxhjlk <epoon0921@bths.edu>
2023-07-21 14:36:26 -0400

    Fix search bar and add markers to map
    Fixed div component for the search bar, also began adding markers to the map for certain buildings

--- webb/src/common/components/mapSample.component.js
+++ webb/src/common/components/mapSample.component.js
@@ -1,6 +1,7 @@
-import { Map, MapContainer, TileLayer } from 'react-leaflet'
+import { useMap, MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import React from 'react';
 import "./mapSample.module.css";
+//import { Marker } from 'leaflet';
 //import L from 'leaflet';
 
 // Try out OSM API calls
@@ -8,21 +9,38 @@ import "./mapSample.module.css";
 
 function MyMap() 
 {
-    const position = [42.7294, -73.6797]
+    const Troy = [42.73101, -73.68051]
+    const Ricketts = [42.73089, -73.6797]
+    const SageLab = [42.73089, -73.68168]
+    const JRowl = [42.72861, -73.68044]
+    const AEaton = [42.73019, -73.6826]
+    const campus_center = [42.7294, -73.6797]
     return (
-        <MapContainer center={position} zoom={17} style={{width:"87%",height:"100vh"}}>
+        <MapContainer center={campus_center} zoom={17} style={{width:"87%",height:"100vh"}}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />;
+            />
+            <Marker position={Troy}>
+                <Popup> Troy Building </Popup>
+            </Marker>
+            <Marker position={Ricketts}>
+                <Popup> Ricketts Building </Popup>
+            </Marker>
+            <Marker position={SageLab}>
+                <Popup> Russell Sage Laboratory  </Popup>
+            </Marker>
+            <Marker position={JRowl}>
+                <Popup> Johnsson Rowland Science Center</Popup>
+            </Marker>
+            <Marker position={AEaton}>
+                <Popup> Amos Eaton </Popup>
+            </Marker>
+
         </MapContainer>
 
     );
 }
 export default MyMap;
 
-            /*, useMap<Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>*/
+            
