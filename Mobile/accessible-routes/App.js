6c18270f9f0c7fc8ee16f28283c235f228d4f8ef
Matthew Merritt <merrittlmatt@gmail.com>
2023-09-26 22:21:25 -0400

    Added header for mobile app

--- Mobile/accessible-routes/App.js
+++ Mobile/accessible-routes/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import MyMap from './common/components/myMap.component';
+import Header from './common/components/header.component';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header/>
       <MyMap/>
     </View>
   );
---
+++ Mobile/accessible-routes/common/components/header.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Header = () => {
+  return <View style={styles.header}>
+    <Text style={styles.titleText}>Accessible Routes</Text>
+  </View>
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    header: {
+      width: '100%',
+      height: '25%',
+    },
+
+    titleText: {
+      fontSize: 20,
+      padding: 120,
+      fontWeight: 'bold',
+    },
+  });
+  
+export default Header;
