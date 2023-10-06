94f3dc8c57ac2663e34b6760dc3ebbefc0cc5b37
Zxhjlk <epoon0921@bths.edu>
2023-10-06 17:35:53 -0400

    Change search bars
    Changed search bars to use react select for easier implementation

--- Website/webb/src/App.js
+++ Website/webb/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MyMap from './common/components/mapSample.component';
 //import searchBar from './common/components/searchBar';
 import {React, useState } from 'react';
+import Select from 'react-select';
 
 
 function App() {
@@ -9,13 +10,53 @@ function App() {
     <div className="Page">
         <MyMap/>
         <div className="Search">
-            <SearchStart/>
-            <SearchDest/>
+            <StartDropDown/>
+            <EndDropDown/>
         </div>
     </div>
   );
 }
 
+const options = [
+  { value: 'test', label: 'Amos Eaton' },
+  { value: 'test2', label: 'DCC'}
+]
+
+const StartDropDown = () => {
+  //Store starting location selected by user to get ready for to query 
+  const [Starting, setStart] = useState('');
+
+  const handleChange = (event) => {
+    setStart(event.label);
+  }
+  return(
+    <div> 
+      <Select 
+      placeholder = 'Choose Starting Location' 
+      onChange = {handleChange}
+      options={options} />
+      <p>{Starting}</p>
+    </div>
+  );
+}
+
+const EndDropDown = () => {
+  //Store destination location selected by user to get ready for query
+  const [Ending, setStart] = useState('');
+
+  const handleChange = (event) => {
+    setStart(event.label);
+  }
+  return(
+    <div> 
+      <Select 
+      placeholder = 'Choose Starting Location' 
+      onChange = {handleChange}
+      options={options} />
+      <p>{Ending}</p>
+    </div>
+  );
+}
 
 const SearchStart = () =>{
   const BarStyle = {width: "14.60vw", height: "4vh", background: "#F0F0F0", border: '3px solid rgba(0, 0, 0, 0.5)', padding: "0.2rem", };
