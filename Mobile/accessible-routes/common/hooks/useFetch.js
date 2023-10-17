208c34a2ed074522b7ed695a37942c1d444bb780
Matthew Merritt <merrittlmatt@gmail.com>
2023-10-17 10:01:09 -0400

    Added useFetch hook

---
+++ Mobile/accessible-routes/common/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url, startLoc, endLoc) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+      try {
+        const response = await fetch(url); // '?startloc=' + startLoc + '&endloc=' + startLoc}
+        if (!response.ok) throw new Error(response.statusText);
+        const json = await response.json();
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (error) {
+        setError(`${error} Could not Fetch Data `);
+        setIsPending(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return { data, isPending, error };
+};
