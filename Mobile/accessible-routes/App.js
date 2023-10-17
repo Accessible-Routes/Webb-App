06442cf469523c1a1050a01ab3b8fabc914b02d5
Matthew Merritt <merrittlmatt@gmail.com>
2023-10-17 18:24:26 -0400

    Added generation of route drawing using mock payload

--- Mobile/accessible-routes/App.js
+++ Mobile/accessible-routes/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
 import MyMap from './common/components/myMap.component';
@@ -6,14 +6,14 @@ import Header from './common/components/header.component';
 import { useFetch } from './common/hooks/useFetch';
 
 
-// mock response from back-end
+// mock response from back-end using points (models start and end locations)
 let mockResponse = {
-  
+
   "route": [
     {
       "index": "1",
       "lat": 42.73020035793919,
-      "long":  -73.68182322241921
+      "long": -73.68182322241921
     },
     {
       "index": "2",
@@ -24,17 +24,45 @@ let mockResponse = {
 }
 
 
+/*
+This is a mock response from the back end to generate a route using polylines:
+
+Notes:
+The structure is an array of objects containing the latitude longitude for each point of a route.
+The first item in the array should be the starting location, and the last location should be the
+ending location for hte route.
+
+*/
+let mockResponse2 = {
+
+  "route": [
+    {
+      "latitude": 42.73020035793919,
+      "longitude": -73.68182322241921
+    },
+    {
+      "latitude": 42.730443933307185,
+      "longitude": -73.68061432268627
+    },
+    {
+      "latitude": 42.730808708361856,
+      "longitude": -73.67975985815578
+    },
+  ]
+}
+
+
 
 export default function App() {
   const [startLocation, setStartLocation] = useState('');
   const [endLocation, setEndLocation] = useState('');
-
+  { console.log('\n\n') }
   return (
     <View style={styles.container}>
-    <SafeAreaView style={{flex:1}}>
-      <Header setStartLocation={setStartLocation} setEndLocation={setEndLocation} />
-    </SafeAreaView>
-      <MyMap routeData={mockResponse} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Header setStartLocation={setStartLocation} setEndLocation={setEndLocation} />
+      </SafeAreaView>
+      <MyMap buildingLocs={mockResponse} routePolyLines={mockResponse2} />
     </View>
   );
 }
--- Mobile/accessible-routes/common/components/myMap.component.js
+++ Mobile/accessible-routes/common/components/myMap.component.js
@@ -1,44 +1,52 @@
 import React from 'react';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import { StyleSheet, View, Text } from 'react-native';
 
-const MyMap = ({routeData}) => {
+const MyMap = ({ buildingLocs, routePolyLines }) => {
   const campus_center = [42.7294, -73.6797]
   const initialRegion = {
-    latitude: 42.729268, 
+    latitude: 42.729268,
     longitude: -73.681227,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   }
 
   return <View style={styles.container}>
-            <MapView 
-              initialRegion={initialRegion}
-              style={styles.map}>
+    <MapView
+      initialRegion={initialRegion}
+      style={styles.map}>
 
-               {routeData.route.map((marker) => 
-                (<Marker
-                  key={marker.index}
-                  coordinate={{latitude: marker.lat, longitude: marker.long}}
-                  title={'loc'}
-                />)
-              )} 
+      {/*Place start and end locations*/}
+      {buildingLocs.route.map((marker) =>
+      (<Marker
+        key={marker.index}
+        coordinate={{ latitude: marker.lat, longitude: marker.long }}
+        title={'loc'}
+      />)
+      )}
 
-              
-            </MapView>
+      {/*Draw route*/}
+      <Polyline
+        coordinates={routePolyLines.route.map((line) => ({ latitude: line.latitude, longitude: line.longitude }))} //specify our coordinates
+        strokeColor={"#000"}
+        strokeWidth={3}
+        lineDashPattern={[1]}
+      />
 
-        </View>
+    </MapView>
+
+  </View>
 };
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 3,
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  });
-  
+  container: {
+    flex: 3,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
 export default MyMap;
