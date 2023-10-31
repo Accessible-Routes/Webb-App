a6d5bba2313dd3f468d665e7a31f43ac399547f2
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2023-10-31 17:29:47 -0400

    update server settings

--- Django/backend/backend/settings.py
+++ Django/backend/backend/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
