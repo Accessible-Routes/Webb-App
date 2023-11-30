d0cee9a91d0228e7ca2afc13af2c777dd8b4a2c3
Matthew Merritt <merrittlmatt@gmail.com>
2023-11-29 22:00:28 -0500

    Refactoring: turned button into a a re-usable component and removed dead code

--- Website/webb/src/App.js
+++ Website/webb/src/App.js
@@ -1,35 +1,11 @@
 import './App.css';
-import MyMap from './common/components/mapSample.component';
-//import searchBar from './common/components/searchBar';
-import {React, useState, useEffect} from 'react';
-import Select from 'react-select';
-import {AwesomeButtonProgress} from 'react-awesome-button';
+import {React} from 'react';
 import 'react-awesome-button/dist/styles.css'; 
-import axios from 'axios';
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-//import Accessibility from "./pages/accessibility";
 import MapPage from "./pages/Map.js";
 import Accessible from "./pages/accessibility.js"
 
-
-/*
-function App() {
-  Call();
-  return (
-    <div className="Page">
-        <MyMap/>
-        <a href="https://google.com">
-          <button id="mapButton">Swap to Accessibility View</button>
-        </a>
-        <div className="Search">
-            <StartDropDown/>
-            <EndDropDown/>
-            <Button/>
-        </div>
-    </div>
-  );
-}*/
 
 function App() {
   return(
--- Website/webb/src/pages/Map.js
+++ Website/webb/src/pages/Map.js
@@ -1,143 +1,110 @@
 import './page.css';
 import MyMap from '../common/components/mapSample.component';
 //import searchBar from './common/components/searchBar';
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import Select from 'react-select';
-import {AwesomeButtonProgress} from 'react-awesome-button';
-import 'react-awesome-button/dist/styles.css'; 
+import { AwesomeButtonProgress } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
 import axios from 'axios';
+import Button from '../common/components/Button.component';
 
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-/*
-export default function mapPage() 
-{
-    Call();
-    return (
-        <div className="Page">
-          <MyMap/>
-          <a href="https://google.com">
-            <button id="mapButton">Swap to Accessibility View</button>
-          </a>
-          <div className="Search">
-              <StartDropDown/>
-              <EndDropDown/>
-              <Button/>
-          </div>
-      </div>
-    );
-}*/
+
+var buildings = []
+ 
+const isAccessible = new Map()
 
 const MapPage = () => {
+  // rendering code:
   Call();
   return (
     <div className="Page">
-      <MyMap/>
-        <button id="mapButton">
-          <Link to ="/accessibility"> Swap to Accessibility View </Link>
-        </button>
+      <MyMap />
+      <button id="mapButton">
+        <Link to="/accessibility"> Swap to Accessibility View </Link>
+      </button>
       <div className="Search">
-          <StartDropDown/>
-          <EndDropDown/>
-          <Button/>
+        <StartDropDown />
+        <EndDropDown />
+        <Button title={"find route"} onPressIn={()=>{console.log('pressed find route button')}} />
       </div>
     </div>
   );
 }
-  
 
-const Button = () => {
-    return (
-      <AwesomeButtonProgress type="secondary"
-        onPress={async (element, next)=>{
-          next();
-        }}>
-          Start
-      </AwesomeButtonProgress>
-    );
+
+function containsBuilding(obj, list) {
+  var i;
+  for (i = 0; i < list.length; i++) {
+    if (list[i].label === obj.Name) {
+      return true;
+    }
   }
-  
-  var buildings = []
+  return false;
+}
 
-  const isAccessible = new Map()
-  
-  function containsBuilding(obj, list) {
-    var i;
-    for (i = 0; i < list.length; i++) {
-        if (list[i].label === obj.Name) {
-            return true;
+const Call = () => {
+  buildings = []
+  axios
+    .get('http://13.56.159.146:8000/api/all-buildings')
+    .then((result) => {
+      for (let i = 0; i < result.data.length; i++) {
+        console.log(containsBuilding(result.data[i], buildings))
+        console.log(result.data[i], buildings)
+        if (containsBuilding(result.data[i], buildings) == false) {
+          buildings.push({
+            value: result.data[i].UUID,
+            label: result.data[i].Name
+          })
+          isAccessible.set(result.data[i].Name, result.data[i].accessible)
         }
-    }
-    return false;
-  }
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+}
 
-  const Call = () => {
-    buildings = []
-    axios
-      .get('http://13.56.159.146:8000/api/all-buildings')
-      .then((result) => {
-        for (let i = 0; i < result.data.length; i++){
-          console.log(containsBuilding(result.data[i], buildings))
-          console.log(result.data[i], buildings)
-          if (containsBuilding(result.data[i], buildings) == false){
-            buildings.push({
-                value: result.data[i].UUID,
-                label: result.data[i].Name
-            })
-            isAccessible.set(result.data[i].Name, result.data[i].accessible)
-          }
-        }
-      }).catch((err) => {
-        console.log(err)
-      })
-  }
 
-const customStyles = {
-    indicatorSeparator: styles => ({ ...styles, display: "none" }),
-      option: (provided, state) => ({
-        ...provided,
-      })
+const StartDropDown = () => {
+  //Store starting location selected by user to get ready for to query 
+  const [Starting, setStart] = useState('');
+
+  const handleChange = (selectedOption) => {
+    console.log(selectedOption)
   }
-  
-  const StartDropDown = () => {
-    //Store starting location selected by user to get ready for to query 
-    const [Starting, setStart] = useState('');
-  
-    const handleChange = (event) => {
-      setStart(event.label);
-    }
-    return(
-      <div> 
-        <Select 
-        menuPortalTarget={document.body} 
-        menuPosition = {'fixed'}
-        placeholder = 'Choose Starting Location' 
-        onChange = {handleChange}
+  return (
+    <div>
+      <Select
+        menuPortalTarget={document.body}  
+        menuPosition={'fixed'}
+        placeholder='Choose Starting Location'
+        onChange={handleChange}
         options={buildings} />
-      </div>
-    );
-    //<p>{Starting}</p>
+    </div>
+  );
+  //<p>{Starting}</p>
+}
+
+const EndDropDown = () => {
+  //Store destination location selected by user to get ready for query
+  const [Ending, setEnd] = useState('');
+
+  const handleChange = (event) => {
+    setEnd(event.label);
   }
-  
-  const EndDropDown = () => {
-    //Store destination location selected by user to get ready for query
-    const [Ending, setEnd] = useState('');
-  
-    const handleChange = (event) => {
-      setEnd(event.label);
-    }
-    return(
-      <div> 
-        <Select 
-        menuPortalTarget={document.body} 
-        menuPosition = {'fixed'}
-        placeholder = 'Choose Starting Location' 
-        onChange = {handleChange}
+  return (
+    <div>
+      <Select
+        menuPortalTarget={document.body}
+        menuPosition={'fixed'}
+        placeholder='Choose Starting Location'
+        onChange={handleChange}
         options={buildings} />
-      </div>
-    );
-    //<p>{Ending}</p>
-  }
+    </div>
+  );
+  //<p>{Ending}</p>
+}
 
 export default MapPage
-export {buildings, isAccessible};
+export { buildings, isAccessible };
