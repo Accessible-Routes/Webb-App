e06ec9a4008738a29ec84e832ce7afc565ebafe9
Matthew Merritt <merrittlmatt@gmail.com>
2023-12-01 23:07:52 -0500

    Refactor Map component to optimize rendering performance and improved aesthetic
    The changes include updating
    the URL for the tile layer to use a more optimized OpenStreetMap source
    and optimizing the key used for each building marker. These improvements
    will result in a better user experience when interacting with the map.

--- Website/webb/src/common/components/Map.js
+++ Website/webb/src/common/components/Map.js
@@ -2,49 +2,30 @@ import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet'
 import React from 'react';
 
 
-const Map = ({buildingLocations, routeCordList}) => {
+const Map = ({ buildingLocations, routeCordList }) => {
     const campus_center = [42.7294, -73.6797];
 
-    console.log('has route in map', routeCordList)
-    console.log(routeCordList)
-
-    console.log('has buildings in map', buildingLocations)
-    console.log(buildingLocations)
-
-    // console.log(buildingLocations)
-    // console.log(routeCordList)
     return (
         <MapContainer center={campus_center} zoom={17} style={{ width: "100%", height: "100vh" }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
             />
 
             {/* Draw buildings */
                 buildingLocations.map((marker) =>
                 (<Marker
-                    // key={marker.location_type}
-                    position={[Number(marker.latitude), Number(marker.longitude) ]}
-                    // title={'temp'}
-                />)
-                )
-                }
+                    key={marker.latitude}
+                    position={[marker.latitude, marker.longitude]}
+                />))
+            }
 
             {/* Draw route */
-            // console.log('aaa');
-            
-            <Polyline positions={routeCordList} color={'red'} />
-                // <Polyline
-                //     coordinates={routeCordList.map((line) => ({ latitude: line.latitude, longitude: line.longitude }))}
-                //     strokeColor={"#FFFF00"}
-                //     strokeWidth={3}
-                //     lineJoin={"round"}
-                // />
-                }
-
+                <Polyline positions={routeCordList} color={'red'} />
+            }
         </MapContainer>
     );
-}
+};
 
 export default Map;
 
