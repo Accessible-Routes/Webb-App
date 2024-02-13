2ff76bae5848bf5bb2f147a002c2ee48bbc923e9
bcritika <58982008+bcritika@users.noreply.github.com>
2024-02-13 17:31:50 -0500

    Added a temporary link for the form

--- Website/webb/src/pages/issue.js
+++ Website/webb/src/pages/issue.js
@@ -1,20 +1,24 @@
 import React from "react";
- 
+
 const Issue = () => {
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "cemter",
-                height: "100vh",
-            }}
-        >
-            <h1>
-                Use this form to report any issues you have: form
-            </h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1 style={{ marginBottom: "20px", textAlign: "center" }}>
+        Use this form to report any issues you have:
+      </h1>
+      <a href="https://stackoverflow.com/questions/50350085/how-to-make-a-hyperlink-external-in-react" rel="noreferrer">
+        FORM
+      </a>
+    </div>
+  );
 };
- 
+
 export default Issue;
