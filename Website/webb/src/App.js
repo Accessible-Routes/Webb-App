52a511a2b4d54169c1b993f7ba498d1a4d8918ab
bcritika <58982008+bcritika@users.noreply.github.com>
2024-02-13 17:06:24 -0500

    basic report issue tab

--- Website/webb/src/App.js
+++ Website/webb/src/App.js
@@ -3,6 +3,7 @@ import 'react-awesome-button/dist/styles.css';
 import CustomNavBar from './common/components/CustomNavBar.component.js';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
+import Issue from "./pages/issue.js";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <CustomNavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/issue" element={<Issue />} />
       </Routes>
     </Router>
   );
---
+++ Website/webb/src/pages/issue.js
@@ -0,0 +1,20 @@
+import React from "react";
+ 
+const Issue = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "cemter",
+                height: "100vh",
+            }}
+        >
+            <h1>
+                Use this form to report any issues you have: form
+            </h1>
+        </div>
+    );
+};
+ 
+export default Issue;
