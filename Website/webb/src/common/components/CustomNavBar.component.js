e136e7db29777accb018337c5a3a127400979416
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-03-15 17:19:23 -0400

    prototype of collapsing Navbar

--- Website/webb/src/common/components/CustomNavBar.component.js
+++ Website/webb/src/common/components/CustomNavBar.component.js
@@ -9,30 +9,34 @@ function CustomNavBar() {
   return (
     <Navbar bg="light" expand="lg" className={styles.navbar}>
       <Container className={styles.navbarContainer}>
-        <Navbar.Brand as={NavLink} to="/">
-          Accessible Routes
-        </Navbar.Brand>
-        <img
-          width="45px"
-          height="40px"
-          alt="Accessible Routes logo"
-          src={require("./Logo.png")}
-        />
-        <Nav className="me-auto">
-          <Nav.Link as={NavLink} to="/" className={styles.navLink}>
-            Home
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/about" className={styles.navLink}>
-            About
-          </Nav.Link>
-          <Nav.Link
-            as={NavLink}
-            to={CONFIG.report_issue_page_link}
-            className={styles.navLink}
-          >
-            Contact Us
-          </Nav.Link>
-        </Nav>
+        
+        <Navbar.Collapse>
+          <Navbar.Brand as={NavLink} to="/">
+            Accessible Routes
+          </Navbar.Brand>
+          <img
+            width="45px"
+            height="40px"
+            alt="Accessible Routes logo"
+            src={require("./Logo.png")}
+          />
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/" className={styles.navLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about" className={styles.navLink}>
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to={CONFIG.report_issue_page_link}
+              className={styles.navLink}
+            >
+              Contact Us
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Toggle className="navbar-toggler">Toggle</Navbar.Toggle>
       </Container>
     </Navbar>
   );
