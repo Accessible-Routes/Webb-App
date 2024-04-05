0b43e99f3a974c072414bf638281b1ae9633fd31
bcritika <58982008+bcritika@users.noreply.github.com>
2024-04-05 17:06:45 -0400

    drop down now works

--- Website/webb/src/common/components/BuildingDropdown.component.js
+++ Website/webb/src/common/components/BuildingDropdown.component.js
@@ -1,33 +1,26 @@
 import Select from 'react-select';
 
 const BuildingDropdown = ({ place_holder_text, building_options, setSelectedBuilding }) => {
-  // component for allowing a user to select a building from building options
-
   const handleChange = (selectedOption) => {
-    setSelectedBuilding(selectedOption)
-  }
-
-  // const customStyles = {
-  //   control: (provided, state) => ({
-  //     flex: 1, 
-  //     position: 'absolute',
-  //     ...provided, // Spread provided styles to retain default styles
-  //   }),
-  // };
-
+    setSelectedBuilding(selectedOption);
+  };
 
   return (
     <div id={place_holder_text}>
       <Select
-        //menuPortalTarget={document.body}
-
+        menuPortalTarget={document.body}
         menuPosition={'fixed'}
         placeholder={place_holder_text}
         onChange={handleChange}
-        options={building_options} />
-        {/* styles={customStyles}  */}
+        options={building_options}
+        styles={{
+          menuPortal: base => ({ ...base, zIndex: 9999 }), // Apply high z-index to the menu
+          menu: provided => ({ ...provided, zIndex: 9999 }), // Apply high z-index to the dropdown menu
+          option: provided => ({ ...provided, zIndex: 9999 }) // Apply high z-index to the dropdown options
+        }}
+      />
     </div>
   );
-}
+};
 
 export default BuildingDropdown;
